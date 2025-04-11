1361173667bb4e717ef055ca829049b087aee7b0
nickvehikl <n.whitford@vehikl.com>
2025-04-11 17:00:27 -0400

    Add icons to nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,10 +1,18 @@
 import {Link} from 'react-router'
+import {AiOutlineDashboard} from 'react-icons/ai'
+import {GoGear} from 'react-icons/go'
 
 export function Nav() {
     return (
-        <nav className="flex justify-center gap-4">
-            <Link to={'/'}>Dashboard</Link>
-            <Link to={'/admin'}>Admin</Link>
+        <nav className="flex justify-end gap-4">
+            <div className="flex items-center gap-1 text-gray-400 hover:text-gray-700">
+                <AiOutlineDashboard/>
+                <Link to={'/'}>Dashboard</Link>
+            </div>
+            <div className="flex items-center gap-1 text-gray-400 hover:text-gray-700">
+                <GoGear/>
+                <Link to={'/admin'}>Admin</Link>
+            </div>
         </nav>
     )
 }
