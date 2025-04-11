89cdbc960091a90db64197286e9c89065b4c32e3
nickvehikl <n.whitford@vehikl.com>
2025-04-11 17:00:19 -0400

    add container to wrap app

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export function Layout({children}: { children: React.ReactNode }) {
             <Meta/>
             <Links/>
         </head>
-        <body>
+        <body className="container mx-auto">
         {children}
         <ScrollRestoration/>
         <Scripts/>
