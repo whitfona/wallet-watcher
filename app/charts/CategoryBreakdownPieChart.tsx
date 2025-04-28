db6efb29dbddf27a6d2d638184729dfa6ddbc547
Nick Whitford <n.whitford@vehikl.com>
2025-04-27 20:45:50 -0400

    Create custom pie chart

---
+++ app/charts/CategoryBreakdownPieChart.tsx
@@ -0,0 +1,37 @@
+import {Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip} from 'recharts'
+
+interface CategoryBreakdownPieChartProps {
+    displayData: { name: string, value: number }[]
+}
+
+export const CategoryBreakdownPieChart = ({displayData}: CategoryBreakdownPieChartProps) => {
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#8884D8', '#82CA9D']
+
+    const DEFAULT_DATA = [
+        {name: 'No Data', value: 100},
+    ]
+
+    const dataToDisplay = displayData && displayData.length > 0 ? displayData : DEFAULT_DATA
+
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+                <Pie
+                    data={dataToDisplay}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={160}
+                    fill="#8884d8"
+                >
+                    {dataToDisplay.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+                    ))}
+                </Pie>
+                <Tooltip formatter={(value) => `$${value}`}/>
+                <Legend layout="vertical" align="right" verticalAlign="middle" iconType="circle"/>
+            </PieChart>
+        </ResponsiveContainer>
+    )
+}
