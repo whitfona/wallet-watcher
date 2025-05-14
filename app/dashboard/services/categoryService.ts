3d003245cc251aa0e4577c06a63fea7e7a3a47f3
Nick Whitford <n.whitford@vehikl.com>
2025-05-14 08:37:30 -0400

    manually categorize expenses

--- app/dashboard/services/categoryService.ts
+++ app/dashboard/services/categoryService.ts
@@ -118,3 +118,194 @@ export const getCategoryTotals = async (expenses: CategoryExpenseRecord[]) => {
         throw new Error('An unexpected error occurred', {cause: err})
     }
 }
+
+export const categorizeExpense = async (expense: ImportedExpense, categories: SelectInterface[]): Promise<SelectInterface | undefined> => {
+    if (expense.category) {
+        const match = categories.find(category =>
+            category.label.toLowerCase().includes(expense.category!.toLowerCase())
+        )
+
+        if (match) {
+            return match
+        }
+    }
+
+    if (!expense.account || !expense.payee) {
+        return undefined
+    }
+
+    const lowerCasePayee = expense.payee.toLowerCase()
+    const isNicksAccount = expense.account && expense.account.toLowerCase().includes('nick')
+    const isJillsAccount = expense.account && expense.account.toLowerCase().includes('jill')
+
+    /**** Amazon ****/
+    if (lowerCasePayee.includes('amazon prime')) {
+        return categories.find(category => category.label.toLowerCase().includes('amazon'))
+    }
+
+    /**** Apple iCloud ****/
+    if (lowerCasePayee.includes('apple bill') &&
+        expense.outflow !== null && expense.outflow === 4.51
+    ) {
+        if (expense.account.toLowerCase().includes('nick')) {
+            return categories.find(category => category.label.toLowerCase().includes('nick apple icloud'))
+        } else {
+            return categories.find(category => category.label.toLowerCase().includes('jill apple icloud'))
+        }
+    }
+
+    /**** Bank Fees ****/
+    if (lowerCasePayee.includes('monthly fee')) {
+        return categories.find(category => category.label.toLowerCase().includes('bank fee'))
+    }
+
+    /**** Coffee/Tea (Timmies) ****/
+    if (lowerCasePayee.includes('tim hortons') ||
+        lowerCasePayee.includes('second cup') ||
+        lowerCasePayee.includes('cafe') ||
+        lowerCasePayee.includes('coffee')
+    ) {
+        return categories.find(category => category.label.toLowerCase().includes('coffee'))
+    }
+
+    /**** Communauto Subscription ****/
+    if (lowerCasePayee.includes('communauto') &&
+        expense?.outflow !== null &&
+        expense.outflow < 7
+    ) {
+        return categories.find(category => category.label.toLowerCase().includes('communauto subscription'))
+    }
+    /**** Communauto Usage ****/
+    if (lowerCasePayee.includes('communauto') &&
+        expense?.outflow !== null &&
+        expense.outflow >= 7
+    ) {
+        return categories.find(category => category.label.toLowerCase().includes('communauto usage'))
+    }
+
+    /**** Costco ****/
+    if (lowerCasePayee.includes('costco')) {
+        return categories.find(category => category.label.toLowerCase().includes('costco'))
+    }
+
+    /**** Essential Health ****/
+    if (lowerCasePayee.includes('optometry')) {
+        return categories.find(category => category.label.toLowerCase().includes('essential'))
+    }
+
+    /**** Gas (Enbridge) ****/
+    if (lowerCasePayee.includes('enbridge')) {
+        return categories.find(category => category.label.toLowerCase().includes('gas'))
+    }
+
+    /**** Groceries ****/
+    if (lowerCasePayee.includes('food basics') || lowerCasePayee.includes('fortino') || lowerCasePayee.includes('zarky')) {
+        return categories.find(category => category.label.toLowerCase().includes('groceries'))
+    }
+
+    /**** Gym ****/
+    if (isNicksAccount && lowerCasePayee.includes('revive')) {
+        return categories.find(category => category.label.toLowerCase().includes('nick gym'))
+    }
+    if (isJillsAccount && lowerCasePayee.includes('revive')) {
+        return categories.find(category => category.label.toLowerCase().includes('jill gym'))
+    }
+
+    /**** Haircuts ****/
+    if (lowerCasePayee.includes('maison fritz') || lowerCasePayee.includes('prime choice')) {
+        return categories.find(category => category.label.toLowerCase().includes('haircut'))
+    }
+
+    /**** Home Insurance (Dublin Intact) ****/
+    if (lowerCasePayee.includes('insurance')) {
+        return categories.find(category => category.label.toLowerCase().includes('insurance'))
+    }
+
+    /**** Hydro (Alectra) ****/
+    if (lowerCasePayee.includes('alectra')) {
+        return categories.find(category => category.label.toLowerCase().includes('hydro'))
+    }
+
+    /**** Internet ****/
+    if (lowerCasePayee.includes('bell canada')) {
+        return categories.find(category => category.label.toLowerCase().includes('internet'))
+    }
+
+    /**** Jill Netflix ****/
+    if (lowerCasePayee.includes('netflix')) {
+        return categories.find(category => category.label.toLowerCase().includes('netflix'))
+    }
+
+    /**** Nespresso ****/
+    if (lowerCasePayee.includes('nespresso')) {
+        return categories.find(category => category.label.toLowerCase().includes('nespresso'))
+    }
+
+    /**** Nick OSAP (NSLSC) ****/
+    if (lowerCasePayee.includes('nslsc')) {
+        return categories.find(category => category.label.toLowerCase().includes('osap'))
+    }
+
+    /**** Phone Bills ****/
+    if (isNicksAccount && lowerCasePayee.includes('koodo')) {
+        return categories.find(category => category.label.toLowerCase().includes('nick phone'))
+    }
+    if (isJillsAccount && lowerCasePayee.includes('koodo')) {
+        return categories.find(category => category.label.toLowerCase().includes('jill phone'))
+    }
+
+    /**** Preventative Health ****/
+    if (lowerCasePayee.includes('osteopathy') ||
+        lowerCasePayee.includes('wellness collectiv') ||
+        lowerCasePayee.includes('wellness inc')
+    ) {
+        return categories.find(category => category.label.toLowerCase().includes('preventative'))
+    }
+
+    /**** Retirement ****/
+    if (lowerCasePayee.includes('questrade')) {
+        return categories.find(category => category.label.toLowerCase().includes('nick retirement'))
+    }
+    if (lowerCasePayee.includes('mutual funds')) {
+        return categories.find(category => category.label.toLowerCase().includes('jill retirement'))
+    }
+    
+    /**** Salary ****/
+    if (lowerCasePayee.includes('vehikl')) {
+        return categories.find(category => category.label.toLowerCase().includes('nick salary'))
+    }
+    if (lowerCasePayee.includes('mcmaster univ - payroll deposit')) {
+        return categories.find(category => category.label.toLowerCase().includes('jill salary'))
+    }
+
+    /**** Sobi ****/
+    if (lowerCasePayee.includes('sobi')) {
+        return categories.find(category => category.label.toLowerCase().includes('sobi'))
+    }
+
+    /**** Spotify ****/
+    if (lowerCasePayee.includes('spotify')) {
+        return categories.find(category => category.label.toLowerCase().includes('spotify'))
+    }
+
+    /**** Sweat App ****/
+    if (lowerCasePayee.includes('sweat')) {
+        return categories.find(category => category.label.toLowerCase().includes('sweat'))
+    }
+
+    /**** Transportation (bus/train/rental car/Uber) ****/
+    if (lowerCasePayee.includes('uber') ||
+        lowerCasePayee.includes('shell') ||
+        lowerCasePayee.includes('presto') ||
+        lowerCasePayee.includes('esso')
+    ) {
+        return categories.find(category => category.label.toLowerCase().includes('transportation'))
+    }
+
+    /**** Website Hosting Fees ****/
+    if (lowerCasePayee.includes('digital ocean') || lowerCasePayee.includes('hostpapa')) {
+        return categories.find(category => category.label.toLowerCase().includes('website hosting'))
+    }
+
+    return undefined
+}
