c5a8ef50d1cc7de82bc4233feaf2412dbcb20a2a
Nick Whitford <n.whitford@vehikl.com>
2025-05-22 09:03:21 -0400

    install vite-plugin-react-router

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import tailwindcss from '@tailwindcss/vite'
 import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
     resolve: {
         alias: {'@': path.resolve(__dirname, 'app')},
     }
